7436eb8d947183e21bd0c981ce13a0ee453f5caf
rebi13 <rebi13@naver.com>
2023-11-09 03:17:56 +0900

    feat: datepicker, status 영역 퍼블리싱

---
+++ client/src/components/views/Todos/TodoForm/TodoStatusGroup/TodoStatusGroup.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="todoStatusGroupWrap">
+    <TodoDatePicker></TodoDatePicker>
+    <TodoStatus></TodoStatus>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import TodoDatePicker from "./TodoDatePicker/TodoDatePicker.vue";
+import TodoStatus from "./TodoStatus/TodoStatus.vue";
+</script>
+<style lang="scss">
+@import "./TodoStatusGroup.scss";
+</style>
