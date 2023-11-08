88e66d996940c7116361b7da2557e25cb5ccf50c
rebi13 <rebi13@naver.com>
2023-11-09 03:14:16 +0900

    fix: 버튼 스타일 추가 및 퍼블리싱 완료

--- client/src/components/views/Todos/AddButton/AddButton.vue
+++ client/src/components/views/Todos/AddButton/AddButton.vue
@@ -1,5 +1,11 @@
 <template>
-  <v-btn icon="mdi-plus" size="small"></v-btn>
+  <div class="addButtonWrap">
+    <v-btn icon="mdi-plus" size="small"></v-btn>
+  </div>
 </template>
 
 <script lang="ts" setup></script>
+
+<style lang="scss">
+@import "./AddButton.scss";
+</style>
