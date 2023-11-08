b0e94fe743c0e54db3b73ff5050fa6279cb0e08f
rebi13 <rebi13@naver.com>
2023-11-09 03:12:19 +0900

    fix: vue datepicker 추가

--- client/src/main.ts
+++ client/src/main.ts
@@ -13,11 +13,14 @@ import { createApp } from "vue";
 // Libaries
 import VueApexCharts from "vue3-apexcharts";
 
+import VueDatePicker from "@vuepic/vue-datepicker";
+import "@vuepic/vue-datepicker/dist/main.css";
+
 // Plugins
 import { registerPlugins } from "@/plugins";
 
 const app = createApp(App);
-
+app.component("VueDatePicker", VueDatePicker);
 app.use(VueApexCharts);
 
 registerPlugins(app);
