cbe5fa2696391a395a93d846dbfb022465ef632a
rebi13 <rebi13@naver.com>
2023-11-09 03:13:46 +0900

    feat: chart 래핑용 파일 추가

---
+++ client/src/components/views/Stats/ChartForm/ChartForm.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <LineChart></LineChart>
+    <DonutChart></DonutChart>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import LineChart from "@/components/views/Stats/ChartForm/LineChart/LineChart.vue";
+import DonutChart from "@/components/views/Stats/ChartForm/DonutChart/DonutChart.vue";
+</script>
+
+<style lang="scss">
+div {
+  width: inherit;
+}
+</style>
