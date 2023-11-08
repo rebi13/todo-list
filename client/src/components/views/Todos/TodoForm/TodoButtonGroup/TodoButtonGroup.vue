026440bb4d2603007ed85b44bcf39e5b533fae9f
rebi13 <rebi13@naver.com>
2023-11-09 03:17:17 +0900

    feat: 추가,취소 버튼 컴포넌트 퍼블리싱

---
+++ client/src/components/views/Todos/TodoForm/TodoButtonGroup/TodoButtonGroup.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="buttonGroupWrap">
+    <Button :name="'추가'" :isAdd="true"></Button>
+    <Button :name="'취소'" :isAdd="false"></Button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import Button from "@/components/common/Button/Button.vue";
+</script>
+<style lang="scss">
+@import "./TodoButtonGroup.scss";
+</style>
