0520b88ac08ddf926adba969e12dfb1e8f38f34d
rebi13 <rebi13@naver.com>
2023-11-09 03:14:36 +0900

    fix: select 영역 임시 커밋

--- client/src/components/views/Todos/SearchBox/Select/Select.vue
+++ client/src/components/views/Todos/SearchBox/Select/Select.vue
@@ -1,5 +1,18 @@
 <template>
-  <v-select :items="['제목', '설명', '상태', '날짜']"></v-select>
+  <v-select
+    class="selectBox"
+    v-model="defaultItem"
+    :items="['제목', '설명', '상태', '날짜']"
+    variant="solo"
+  ></v-select>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const defaultItem = ref("제목");
+</script>
+
+<style lang="scss">
+@import "./Select.scss";
+</style>
