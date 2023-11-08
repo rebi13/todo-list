b59df55d77465ec601efb2fcc328f23e37c8dbce
rebi13 <rebi13@naver.com>
2023-11-08 17:21:21 +0900

    fix: 영역 구성 및 스타일 추가

--- client/src/layouts/default/View.vue
+++ client/src/layouts/default/View.vue
@@ -1,12 +1,41 @@
 <template>
-  <v-main>
-    <h1>TODO List</h1>
-    <router-link to="/">목록</router-link>
-    <router-link to="/stats">통계</router-link>
-    <router-view />
+  <v-main class="wrap">
+    <div class="container">
+      <h1>TODO List</h1>
+      <nav class="nav">
+        <router-link to="/">목록</router-link>
+        <router-link to="/stats">통계</router-link>
+      </nav>
+      <router-view />
+    </div>
   </v-main>
 </template>
 
 <script lang="ts" setup>
 //
 </script>
+
+<style lang="scss">
+.wrap {
+  width: 100%;
+  max-width: 60rem !important;
+  margin: 1rem auto;
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.nav {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  width: 20rem;
+  padding: 1rem;
+  a {
+    text-decoration: none;
+  }
+}
+</style>
