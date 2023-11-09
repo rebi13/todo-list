0a58bb2740226e224919129bb821a8a752d55ba9
rebi13 <rebi13@naver.com>
2023-11-09 17:13:23 +0900

    fix: todo 제목, 내용 부모자식 데이터 연결

--- client/src/components/views/Todos/TodoForm/TodoForm.vue
+++ client/src/components/views/Todos/TodoForm/TodoForm.vue
@@ -1,16 +1,43 @@
 <template>
   <div class="todoFormWrap">
-    <TodoInput label="제목을 입력하세요."></TodoInput>
-    <TodoInput label="설명을 입력하세요."></TodoInput>
+    <TodoInput
+      label="제목을 입력하세요."
+      :data="'title'"
+      @input-changed="handleInputChange"
+    ></TodoInput>
+    <TodoInput
+      label="설명을 입력하세요."
+      :data="'content'"
+      @input-changed="handleInputChange"
+    ></TodoInput>
     <TodoStatusGroup></TodoStatusGroup>
     <TodoButtonGroup></TodoButtonGroup>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { ref } from "vue";
 import TodoButtonGroup from "./TodoButtonGroup/TodoButtonGroup.vue";
 import TodoInput from "./TodoInput/TodoInput.vue";
 import TodoStatusGroup from "./TodoStatusGroup/TodoStatusGroup.vue";
+
+const titleInput = ref("");
+const contentInput = ref("");
+
+const handleInputChange = (input: string, data: string) => {
+  switch (data) {
+    case "title":
+      titleInput.value = input;
+      break;
+    case "content":
+      contentInput.value = input;
+      break;
+    case "date":
+      break;
+    case "status":
+      break;
+  }
+};
 </script>
 
 <style lang="scss">
--- client/src/components/views/Todos/TodoForm/TodoInput/TodoInput.vue
+++ client/src/components/views/Todos/TodoForm/TodoInput/TodoInput.vue
@@ -3,18 +3,31 @@
     class="textField"
     :label="label"
     variant="underlined"
+    v-model="inputData"
+    @blur="sendDataToParent"
   ></v-text-field>
 </template>
 
 <script lang="ts" setup>
-import { defineProps } from "vue";
+import { ref, defineProps, defineEmits } from "vue";
 
 const props = defineProps({
   label: {
     type: String,
     required: true,
   },
+  data: {
+    type: String,
+    required: true,
+  },
 });
+// const { data } = props;
+const inputData = ref("");
+const emit = defineEmits(["input-changed"]);
+
+const sendDataToParent = () => {
+  emit("input-changed", inputData.value, props.data);
+};
 </script>
 
 <style lang="scss">
