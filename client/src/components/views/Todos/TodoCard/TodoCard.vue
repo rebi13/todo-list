fb55ec21812ec58839cec1f137bce283f35f9fa7
rebi13 <rebi13@naver.com>
2023-11-09 15:17:34 +0900

    feat: todo 내역 퍼블리싱 임시 커밋

---
+++ client/src/components/views/Todos/TodoCard/TodoCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="todoCard">
+    <div class="todoInfo">
+      <h1>title</h1>
+      <div>content</div>
+    </div>
+    <div class="todoStatus">
+      <div>날짜</div>
+      <div>상태</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss">
+@import "./TodoCard.scss";
+</style>
