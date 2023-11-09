1994a9673e6769f79ef7e07c52cebff40179e72f
rebi13 <rebi13@naver.com>
2023-11-09 16:28:23 +0900

    fix: todo card 정보 조회 및 데이터 바인딩

--- client/src/components/views/Todos/Todos.vue
+++ client/src/components/views/Todos/Todos.vue
@@ -2,16 +2,26 @@
   <div class="todoContent">
     <SearchBox></SearchBox>
     <AddButton></AddButton>
-    <TodoCard></TodoCard>
+    <TodoCard v-for="item in todoList" :todo="item"></TodoCard>
     <TodoForm></TodoForm>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { ref, onMounted } from "vue";
+
 import SearchBox from "./SearchBox/SearchBox.vue";
 import AddButton from "./AddButton/AddButton.vue";
 import TodoForm from "./TodoForm/TodoForm.vue";
 import TodoCard from "./TodoCard/TodoCard.vue";
+import axiosRequest from "@/api/index";
+
+const todoList = ref([]);
+
+onMounted(async () => {
+  const response = await axiosRequest.requestAxios<any>("get", "/todos");
+  todoList.value = response.data;
+});
 </script>
 
 <style lang="scss">
