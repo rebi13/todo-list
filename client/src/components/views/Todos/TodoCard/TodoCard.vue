d87d3253b2faab9064cdf67857c2843405c6e0e6
rebi13 <rebi13@naver.com>
2023-11-09 16:29:03 +0900

    fix: todo 내용 데이터 바인딩

--- client/src/components/views/Todos/TodoCard/TodoCard.vue
+++ client/src/components/views/Todos/TodoCard/TodoCard.vue
@@ -1,17 +1,43 @@
 <template>
   <div class="todoCard">
     <div class="todoInfo">
-      <h1>title</h1>
-      <div>content</div>
+      as
+      <h1>{{ todo.title }}</h1>
+      <div>{{ todo.content }}</div>
     </div>
     <div class="todoStatus">
-      <div>날짜</div>
-      <div>상태</div>
+      <div>{{ todo.createDate }}</div>
+      <div>{{ todo.status }}</div>
     </div>
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  todo: {
+    type: Object,
+    required: true,
+    // title: {
+    //   type: String,
+    //   required: true,
+    // },
+    // content: {
+    //   type: String,
+    //   required: true,
+    // },
+    // createDate: {
+    //   type: String,
+    //   required: true,
+    // },
+    // status: {
+    //   type: String,
+    //   required: true,
+    // },
+  },
+});
+</script>
 
 <style lang="scss">
 @import "./TodoCard.scss";
