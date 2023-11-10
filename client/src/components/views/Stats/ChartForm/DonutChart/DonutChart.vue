0e5b0d3cb2767d7dbde922f5804766ea6be1baab
rebi13 <rebi13@naver.com>
2023-11-11 00:47:51 +0900

    feat: 도넛차트 데이터 표시

--- client/src/components/views/Stats/ChartForm/DonutChart/DonutChart.vue
+++ client/src/components/views/Stats/ChartForm/DonutChart/DonutChart.vue
@@ -3,39 +3,58 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted } from "vue";
+import { computed, onMounted, watch } from "vue";
 import * as d3 from "d3";
+import { useStore } from "vuex";
+import { todo } from "@/@types/index";
 
-const data = [
-  { country: "USA", value: 20.5 },
-  { country: "China", value: 13.4 },
-  { country: "Germany", value: 4.0 },
-  { country: "Japan", value: 4.9 },
-  { country: "France", value: 2.8 },
-];
+const store = useStore();
+const data = computed(() => store.state.todos);
+const statusCounts: { [key: string]: number } = {};
+
+data.value.forEach((item: todo) => {
+  const status = item.status;
+  statusCounts[status] = (statusCounts[status] || 0) + 1;
+});
+
+const chartData = Object.entries(statusCounts).map(([status, count]) => ({
+  status: status,
+  count: count,
+}));
 
 const width = 600;
 const height = 350;
 const radius = Math.min(width, height) / 2;
 
 onMounted(() => {
+  // watch for changes in the 'data' and update the chart
+  watch(data, () => {
+    setDonutChart();
+  });
+
+  // initial chart rendering
+  setDonutChart();
+});
+const chartColor: any = {
+  진행전: "red",
+  진행중: "blue",
+  완료: "green",
+};
+const setDonutChart = () => {
   const svg = d3
     .select("#donutChart")
     .append("svg")
     .attr("width", width)
     .attr("height", height)
     .append("g")
-    .attr("transform", `translate(${width / 2},${height / 2})`);
+    .attr("transform", `translate(${width / 3},${height / 2})`);
 
-  const color = d3.scaleOrdinal(d3.schemeCategory10);
-
-  const pie = d3.pie<{ value: number }>().value((d) => d.value as number);
-  // const pie = d3.pie().value((d: any) => d.value);
+  const pie = d3.pie<{ count: number }>().value((d) => d.count as number);
 
   const arc = d3.arc().innerRadius(50).outerRadius(radius);
   const arcs = svg
     .selectAll("arc")
-    .data(pie(data))
+    .data(pie(chartData))
     .enter()
     .append("g")
     .attr("class", "arc");
@@ -43,7 +62,7 @@ onMounted(() => {
   arcs
     .append("path")
     .attr("d", arc as any)
-    .attr("fill", (d: any, i: any) => color(String(i)))
+    .attr("fill", (d: any) => chartColor[d.data.status])
     .attr("stroke", "white")
     .style("stroke-width", "2px");
 
@@ -51,8 +70,34 @@ onMounted(() => {
     .append("text")
     .attr("transform", (d: any) => `translate(${arc.centroid(d)})`)
     .attr("text-anchor", "middle")
-    .text((d: any) => d.data.country);
-});
+    .text((d: any) => d.data.status)
+    .style("fill", "white");
+
+  // 범례 추가
+  const legend = svg
+    .selectAll(".legend")
+    .data(chartData) // 수정된 부분
+    .enter()
+    .append("g")
+    .attr("class", "legend")
+    .attr("transform", (d, i) => `translate(-40,${i * 30})`);
+
+  legend
+    .append("rect")
+    .attr("x", width / 2 + 10)
+    .attr("y", (d, i) => i * 20)
+    .attr("width", 18)
+    .attr("height", 18)
+    .style("fill", (d) => chartColor[d.status]); // 수정된 부분
+
+  legend
+    .append("text")
+    .attr("x", width / 2 + 35)
+    .attr("y", (d, i) => i * 20 + 9)
+    .attr("dy", ".35em")
+    .style("text-anchor", "start")
+    .text((d: any) => d.status + " " + d.count + "개"); // 수정된 부분
+};
 </script>
 
 <style lang="scss">
