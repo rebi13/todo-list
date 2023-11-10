71a4256c7155bb23a8ed5d1eca20413f36e78f0a
rebi13 <rebi13@naver.com>
2023-11-11 02:10:09 +0900

    feat: 최근 7일간 todo 갯수 linechart 표시

--- client/src/components/views/Stats/ChartForm/LineChart/LineChart.vue
+++ client/src/components/views/Stats/ChartForm/LineChart/LineChart.vue
@@ -9,21 +9,61 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, computed } from "vue";
+import { useStore } from "vuex";
+import dayjs from "dayjs";
+import { todo } from "@/@types/index";
+
+const store = useStore();
+const data = computed(() => store.state.todos);
+// 오늘로부터 7일 전의 날짜 계산
+const sevenDaysAgo = dayjs().subtract(7, "day");
+
+// createDate가 오늘로부터 7일 전까지인 데이터 필터링
+const filteredData = data.value.filter((item: todo) =>
+  dayjs(item.createDate).isAfter(sevenDaysAgo),
+);
+
+// series와 xaxis 데이터 생성
+const seriesData: any[] = [];
+const xaxisData: any = { categories: [] };
+
+// 오늘부터 7일 전까지의 날짜를 xaxis에 추가
+for (let i = 6; i >= 0; i--) {
+  const date: string = dayjs().subtract(i, "day").format("YYYY-MM-DD");
+  xaxisData.categories.push(date);
+
+  if (data.value.length > 0) {
+    // 해당 날짜에 있는 데이터를 찾아서 series에 추가
+    const matchingData = filteredData.filter(
+      (item: todo) => dayjs(item.createDate).format("YYYY-MM-DD") === date,
+    );
+    if (matchingData.length > 0) {
+      // 해당 날짜에 해당하는 todo의 수를 계산하여 series에 추가
+      const todoCount = matchingData.length;
+      seriesData.push(todoCount);
+    } else {
+      // 해당 날짜에 데이터가 없으면 0으로 채웁니다.
+      seriesData.push(0);
+    }
+  }
+}
 
 const series = ref([
   {
-    name: "Desktops",
-    data: [10, 41, 35, 51, 49, 62, 69, 91, 148],
+    name: "Todo 갯수",
+    data: seriesData,
   },
 ]);
-
 const chartOptions = ref({
   chart: {
     height: 350,
     type: "line",
     zoom: {
       enabled: false,
+    },
+    toolbar: {
+      show: false,
     },
   },
   dataLabels: {
@@ -32,18 +72,37 @@ const chartOptions = ref({
   stroke: {
     curve: "straight",
   },
-  title: {
-    text: "Product Trends by Month",
-    align: "left",
-  },
   grid: {
     row: {
-      colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
+      colors: ["#f3f3f3"], // takes an array which will be repeated on columns
       opacity: 0.5,
     },
   },
+
+  tooltip: {
+    y: {
+      formatter: (e: any) => parseInt(e), // 툴팁에만 영향을 미치도록 소수점 제거
+    },
+  },
+  yAxis: {
+    labels: {
+      show: false,
+    },
+  },
+  noData: {
+    text: "no data",
+    align: "center",
+    verticalAlign: "middle",
+    offsetX: 0,
+    offsetY: 0,
+    style: {
+      color: undefined,
+      fontSize: "14px",
+      fontFamily: undefined,
+    },
+  },
   xaxis: {
-    categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep"],
+    categories: xaxisData.categories,
   },
 });
 </script>
