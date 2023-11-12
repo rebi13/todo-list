c671c990993400dc0e277e60b963acb835e63020
rebi13 <rebi13@naver.com>
2023-11-12 21:13:15 +0900

    feat: select, todo status 관리용 상수 추가

---
+++ client/src/constants/index.ts
@@ -0,0 +1,12 @@
+export enum SELECT_STATUS {
+  title = "제목",
+  content = "설명",
+  status = "상태",
+  createDate = "날짜",
+}
+
+export enum TODO_STATUS {
+  before = "진행전",
+  ing = "진행중",
+  complete = "완료",
+}
